b9270ef8def17591350886592b104514932a05f6
Pere Frontera <pere.frontera@gmail.com>
2019-05-01 16:28:54 +0200

    Add tests project

---
+++ NinjaApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace NinjaApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
